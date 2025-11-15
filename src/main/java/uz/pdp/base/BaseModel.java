f59d600d509a8a216ea1bd4a964d66830a3ec037
Muzaffar Xomitaliyev <muzaffarxomitaliyev23@gmail.com>
2025-11-15 17:16:46 +0500

    hello world

--- src/main/java/uz/pdp/base/BaseModel.java
+++ src/main/java/uz/pdp/base/BaseModel.java
@@ -11,10 +11,13 @@ import java.util.UUID;
 @Setter
 public abstract class BaseModel {
     private final UUID id;
+
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SS", timezone = "UTC")
     private final Instant createdAt = Instant.now();
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX", timezone = "UTC")
+
     private Instant updatedAt = Instant.now();
+
     private boolean active = true;
 
     public BaseModel() {
